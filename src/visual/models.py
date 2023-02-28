33fd768bad36c7cae1b2bc6135235132b9aa5557
Thomas Barba <tbarba@bmir-ct-gpu-5.stanford.edu>
2023-02-28 13:13:39 -0800

    before saliency

--- src/visual/models.py
+++ src/visual/models.py
@@ -1,6 +1,114 @@
 import pytorch_lightning as pl
 import torch
 
+import os
+import torch
+import numpy as np
+import torch.nn as nn
+from collections import OrderedDict
+from torch.utils.data import Dataset
+
+
+class UNet3D(nn.Module):
+    def __init__(self, in_channels=1, out_channels=1, init_features=32, num_blocks=5):
+        super(UNet3D, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.num_blocks = num_blocks
+        
+        # ENCODER BLOCKS
+        feature_list = [init_features*(2**n) for n in range(num_blocks)]
+        self.enc_blocks = nn.ModuleList()
+        self.pool = nn.MaxPool3d(kernel_size=2, stride=2)
+        for i, n in enumerate(feature_list):
+            if i ==0:
+                enc_block = UNet3D._block(self.in_channels, n, name=f"enc{i}")
+            else:
+                enc_block = UNet3D._block(feature_list[i-1], n, name=f"enc{i}")
+
+            self.enc_blocks.append(enc_block)
+        
+        # BOTTLENECK EXTRACTION
+        bottleneck_features= 2*feature_list[-1]
+        self.bottleneck = UNet3D._block(feature_list[-1], bottleneck_features, name="bottleneck")
+
+        # DECODER BLOCKS
+        feature_list = feature_list[::-1]
+        self.dec_blocks = nn.ModuleList()
+        self.transposers = nn.ModuleList()
+        for i, n in enumerate(feature_list):
+            if i==0:
+                upconv = nn.ConvTranspose3d(bottleneck_features, n, kernel_size=2, stride=2)
+                dec_block = UNet3D._block(bottleneck_features, n, name=f"dec{i}")
+            else: 
+                upconv = nn.ConvTranspose3d(feature_list[i-1], n, kernel_size=2, stride=2)
+                dec_block = UNet3D._block(feature_list[i-1], n, name=f"dec{i}")
+            
+            self.transposers.append(upconv)
+            self.dec_blocks.append(dec_block)
+
+        # FINAL CONVOLUTION
+        self.last_conv = nn.Conv3d(feature_list[-1], out_channels=out_channels, kernel_size=1)
+
+    def forward(self, x):
+        ## ENCODING
+        encodings = []
+        for k in range(self.num_blocks):
+            enc = self.enc_blocks[k](x)
+            x = self.pool(enc)
+            encodings.append(enc)
+            
+        ## REPRESENTATION EXTRACTION
+        bottleneck = self.bottleneck(x)
+
+        ## DECODING
+        encodings.reverse()
+        for k in range(self.num_blocks):   
+            if k==0:
+                dec = self.transposers[k](bottleneck, output_size= encodings[k].shape)
+            else:
+                dec = self.transposers[k](dec, output_size= encodings[k].shape)
+
+            dec = torch.cat((dec, encodings[k]), dim=1)
+            dec = self.dec_blocks[k](dec)
+
+
+        return torch.sigmoid(self.last_conv(dec)), bottleneck
+
+    @staticmethod
+    def _block(in_channels, features, name):
+        return nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        name + "conv1",
+                        nn.Conv3d(
+                            in_channels=in_channels,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=False,
+                        ),
+                    ),
+                    (name + "norm1", nn.BatchNorm3d(num_features=features)),
+                    (name + "relu1", nn.ReLU(inplace=True)),
+                    (
+                        name + "conv2",
+                        nn.Conv3d(
+                            in_channels=features,
+                            out_channels=features,
+                            kernel_size=3,
+                            padding=1,
+                            bias=False,
+                        ),
+                    ),
+                    (name + "norm2", nn.BatchNorm3d(num_features=features)),
+                    (name + "relu2", nn.ReLU(inplace=True)),
+                ]
+            )
+        )
+
 
 
 
---
+++ src/visual/saliency.py
@@ -0,0 +1,81 @@
+import argparse
+import os
+import torch
+import pandas as pd
+from models import UNet3D
+import torch.nn as nn
+from tqdm import tqdm
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model_name", type=str, help='model name')
+    parser.add_argument("-d", "--model_dir", type=str, default = "outputs/UNet", help='model path')
+    parser.add_argument("-n", "--num_mod", type=int, default = 2, help='number of modalities')
+    parser.add_argument("-f", "--init_feat", type=int, default = 8, help='number of initial features')
+    parser.add_argument("-b", "--num_blocks", type=int, default = 6, help='number of conv blocks')
+
+    args = parser.parse_args()
+    args.model_dir = os.path.join(args.model_dir, args.model_name)
+
+    return args
+
+
+def import_model(model_dir, num_mod, init_feat, num_blocks, device):
+
+    model_path = os.path.join(model_dir, "autoencoding/exported_data/best_model.pt")
+
+    model = UNet3D(in_channels=num_mod, out_channels=num_mod,
+                   init_features=init_feat, num_blocks=num_blocks)
+    model = nn.DataParallel(model)
+    model = model.to(device)
+    model.load_state_dict(torch.load(model_path))
+
+    return model
+
+
+def extract_features(net, val_dataloader, device):
+
+    net.eval()
+    results = {}
+
+    for b_idx, (imgs, cases) in enumerate(tqdm(val_dataloader, colour="blue")):
+        imgs = imgs.to(device)
+        with torch.no_grad():
+            _, extracted_features = net(imgs)
+            extracted_features = extracted_features.detach().cpu()
+
+        for i, case in enumerate(cases):
+            case_features = extracted_features[i,:, :, :, :].reshape(-1).numpy()
+
+            results[case] = case_features
+
+    return results
+
+
+def main():
+    os.chdir("/home/tbarba/projects/MultiModalBrainSurvival")
+    args = parse_arguments()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net = import_model(args.model_dir, args.num_mod, args.init_feat, args.num_blocks, device)
+
+    print("\nLoading datasets...")
+    trainLoader = torch.load(f'{args.model_dir}/autoencoding/exported_data/trainLoader.pth')
+    testLoader = torch.load(f'{args.model_dir}/autoencoding/exported_data/testLoader.pth')
+    dict_loaders = {"train": trainLoader, "test": testLoader}
+
+    # feature extraction
+    os.makedirs(f"{args.model_dir}/autoencoding/features", exist_ok=True)
+    for dataset in ["train", "test"]:
+        print(f"Extracting features for dataset : {dataset}")
+        results = extract_features(
+            net, dict_loaders[dataset], device)
+
+        feature_dict = pd.DataFrame.from_dict(results, orient="index")
+        feature_dict.to_csv(
+            f"{args.model_dir}/autoencoding/features/{dataset}_features.csv.gz", index=True)
+
+
+if __name__ == "__main__":
+    main()
+
+
