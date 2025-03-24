fcbeea7a06b70531f9d28cb3ec177ec68a0a25c2
TB <tbarba19@gmail.com>
2025-03-24 17:22:36 +0100

    version 24 mars - ok

--- oldfiles/src/tools/backup_outputs.sh
+++
@@ -1,9 +0,0 @@
-cd /home/tbarba/projects/MultiModalBrainSurvival/
-
-
-now=$(date +"%Y-%m-%d")
-tar_file="$now-outputs.tar.gz"
-tar -zcvf "$tar_file" data/MR/outputs
-
-rsync -avrP "$tar_file" dtn.sherlock.stanford.edu:/oak/stanford/groups/ogevaert/data/Brain_VAE/
-rm "$tar_file"
--- oldfiles/src/tools/format_datasets/create_MIXED_KC.py
+++
@@ -1,9 +0,0 @@
-import os
-from os.path import join
-import shutil
-from random import sample
-
-
-
-MR_DIR = "/home/tbarba/projects/MultiModalBrainSurvival/data/MR"
-datasets = ["UCSF","UPENN","REMBRANDT"]
